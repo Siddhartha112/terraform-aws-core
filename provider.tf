61962e1ed4c90ffd5cd75285b2ad78842a7aa70b
gopal <gopal1409@gmail.com>
2023-03-27 12:43:25 +0530

    terraform provider

---
+++ provider.tf
@@ -0,0 +1,15 @@
+###we need to create a provider block. 
+##when i run terraform init it will always download the plugin. 
+ terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
